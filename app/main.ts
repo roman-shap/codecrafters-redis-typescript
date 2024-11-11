e9ef529636d2db162c72538d0db868abc92b7f25
Roman Shapira <roman-shap@users.noreply.github.com>
2024-11-11 19:45:25 +0200

    respond to PING

--- app/main.ts
+++ app/main.ts
@@ -2,7 +2,17 @@ import * as net from "net";
 
 
 const server: net.Server = net.createServer((connection: net.Socket) => {
-  // Handle connection
+  connection.on("data", (data) => {
+    const message = data.toString();
+    console.log("Received message:", JSON.stringify(message));
+    const response = `+PONG\r\n`;
+    console.log("Response is:", JSON.stringify(response));
+    connection.write(response);
+  });
+  connection.on("close", () => {
+    console.log("Connection closed");
+    connection.end();
+  });
 });
 
 server.listen(6379, "127.0.0.1");
