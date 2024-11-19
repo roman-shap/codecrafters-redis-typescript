a77f0b278b16e4dd7c921679fe89381b0bd6cabd
Roman Shapira <roman-shap@users.noreply.github.com>
2024-11-19 22:46:49 +0200

    support null bulk strings
    Initialized by not passing any value to the constructor.

--- app/parser.ts
+++ app/parser.ts
@@ -12,7 +12,7 @@ export abstract class RESPNode {
   }
 
   protected serializeValue(): string {
-    return `${this.value}\r\n`;
+    return this.value ? `${this.value}\r\n` : "";
   }
 
   private serializeLength(): string {
@@ -35,9 +35,9 @@ export class SimpleString extends RESPNode {
 export class BulkString extends RESPNode {
   firstByte = "$";
 
-  constructor(value: string) {
+  constructor(value: string | null) {
     super(value);
-    this.length = value.length;
+    this.length = value ? value.length : -1;
   }
 
   public static parse(tokens: string[]): [BulkString, number] {
@@ -46,7 +46,8 @@ export class BulkString extends RESPNode {
   }
 
   private static validateBulkStringTokens(tokens: string[]): void {
-    if (tokens.length < 2 || Number(tokens[0].slice(1)) != tokens[1].length) {
+    if (!((tokens.length === 1 && tokens[0] === "$-1") ||
+      (tokens.length >= 2 && Number(tokens[0].slice(1)) === tokens[1].length))) {
       throw new Error("Invalid BulkString");
     }
   }
